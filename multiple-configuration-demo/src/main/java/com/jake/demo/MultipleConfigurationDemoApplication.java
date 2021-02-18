ba2d1211d927d2958494f32eb349596f08bc71ae
Jake Lin <56834723+jake-tw@users.noreply.github.com>
2021-02-18 11:41:02 +0800

    initial commit

---
+++ multiple-configuration-demo/src/main/java/com/jake/demo/MultipleConfigurationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jake.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleConfigurationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleConfigurationDemoApplication.class, args);
+	}
+
+}
